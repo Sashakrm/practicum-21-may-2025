4c023e2038f4fa4e01b3a06a401060a95bae561c
Sashakrm <sasakramch@gmail.com>
2025-05-21 09:33:40 +0300

    Add files via upload

---
+++ Ex_prac_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_prac_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine($"{x}+{y}={sum}");
+            Console.Read();
+        }
+    }
+}
