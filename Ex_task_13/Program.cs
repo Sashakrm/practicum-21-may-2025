4a539496fbe19f12c972d623f747d33ecdcf9cb7
Sashakrm <sasakramch@gmail.com>
2025-05-21 09:36:07 +0300

    Add files via upload

---
+++ Ex_task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_task_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int input = int.Parse(Console.ReadLine());
+            int dublicat = input;
+            int total_input = input / 10; //получаем десятки
+            int total_dublicat = dublicat % 10;//получаем единицы
+            Console.WriteLine($"{total_input} {total_dublicat}");
+            Console.Read();
+        }
+    }
+}
